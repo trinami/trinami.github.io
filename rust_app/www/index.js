610b0fc332bbef54930c543ad41f4c487bfe4ff6
Stewie <stewie@trinami.org>
2025-05-05 20:36:17 +0200

    feat: trinami page from trinami/trinami.org   hugo output

---
+++ rust_app/www/index.js
@@ -0,0 +1,193 @@
+(() => {
+  // ns-hugo-imp:/home/user/git2/trinami.github.io/assets/rust_app/pkg/rust_app.js
+  var import_meta = {};
+  var wasm;
+  var cachedUint8ArrayMemory0 = null;
+  function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
+      cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+  }
+  function getArrayU8FromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return getUint8ArrayMemory0().subarray(ptr / 1, ptr / 1 + len);
+  }
+  function addToExternrefTable0(obj) {
+    const idx = wasm.__externref_table_alloc();
+    wasm.__wbindgen_export_2.set(idx, obj);
+    return idx;
+  }
+  function handleError(f, args) {
+    try {
+      return f.apply(this, args);
+    } catch (e) {
+      const idx = addToExternrefTable0(e);
+      wasm.__wbindgen_exn_store(idx);
+    }
+  }
+  var cachedInt32ArrayMemory0 = null;
+  var WASM_VECTOR_LEN = 0;
+  var cachedTextEncoder = typeof TextEncoder !== "undefined" ? new TextEncoder("utf-8") : { encode: () => {
+    throw Error("TextEncoder not available");
+  } };
+  var encodeString = typeof cachedTextEncoder.encodeInto === "function" ? function(arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+  } : function(arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+      read: arg.length,
+      written: buf.length
+    };
+  };
+  function passStringToWasm0(arg, malloc, realloc) {
+    if (realloc === void 0) {
+      const buf = cachedTextEncoder.encode(arg);
+      const ptr2 = malloc(buf.length, 1) >>> 0;
+      getUint8ArrayMemory0().subarray(ptr2, ptr2 + buf.length).set(buf);
+      WASM_VECTOR_LEN = buf.length;
+      return ptr2;
+    }
+    let len = arg.length;
+    let ptr = malloc(len, 1) >>> 0;
+    const mem = getUint8ArrayMemory0();
+    let offset = 0;
+    for (; offset < len; offset++) {
+      const code = arg.charCodeAt(offset);
+      if (code > 127) break;
+      mem[ptr + offset] = code;
+    }
+    if (offset !== len) {
+      if (offset !== 0) {
+        arg = arg.slice(offset);
+      }
+      ptr = realloc(ptr, len, len = offset + arg.length * 3, 1) >>> 0;
+      const view = getUint8ArrayMemory0().subarray(ptr + offset, ptr + len);
+      const ret = encodeString(arg, view);
+      offset += ret.written;
+      ptr = realloc(ptr, len, offset, 1) >>> 0;
+    }
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+  }
+  var cachedTextDecoder = typeof TextDecoder !== "undefined" ? new TextDecoder("utf-8", { ignoreBOM: true, fatal: true }) : { decode: () => {
+    throw Error("TextDecoder not available");
+  } };
+  if (typeof TextDecoder !== "undefined") {
+    cachedTextDecoder.decode();
+  }
+  function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
+  }
+  function encrypt_message(message) {
+    let deferred2_0;
+    let deferred2_1;
+    try {
+      const ptr0 = passStringToWasm0(message, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+      const len0 = WASM_VECTOR_LEN;
+      const ret = wasm.encrypt_message(ptr0, len0);
+      deferred2_0 = ret[0];
+      deferred2_1 = ret[1];
+      return getStringFromWasm0(ret[0], ret[1]);
+    } finally {
+      wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
+    }
+  }
+  async function __wbg_load(module, imports) {
+    if (typeof Response === "function" && module instanceof Response) {
+      if (typeof WebAssembly.instantiateStreaming === "function") {
+        try {
+          return await WebAssembly.instantiateStreaming(module, imports);
+        } catch (e) {
+          if (module.headers.get("Content-Type") != "application/wasm") {
+            console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve Wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+          } else {
+            throw e;
+          }
+        }
+      }
+      const bytes = await module.arrayBuffer();
+      return await WebAssembly.instantiate(bytes, imports);
+    } else {
+      const instance = await WebAssembly.instantiate(module, imports);
+      if (instance instanceof WebAssembly.Instance) {
+        return { instance, module };
+      } else {
+        return instance;
+      }
+    }
+  }
+  function __wbg_get_imports() {
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbg_getRandomValues_21a0191e74d0e1d3 = function() {
+      return handleError(function(arg0, arg1) {
+        globalThis.crypto.getRandomValues(getArrayU8FromWasm0(arg0, arg1));
+      }, arguments);
+    };
+    imports.wbg.__wbindgen_init_externref_table = function() {
+      const table = wasm.__wbindgen_export_2;
+      const offset = table.grow(4);
+      table.set(0, void 0);
+      table.set(offset + 0, void 0);
+      table.set(offset + 1, null);
+      table.set(offset + 2, true);
+      table.set(offset + 3, false);
+      ;
+    };
+    return imports;
+  }
+  function __wbg_init_memory(imports, memory) {
+  }
+  function __wbg_finalize_init(instance, module) {
+    wasm = instance.exports;
+    __wbg_init.__wbindgen_wasm_module = module;
+    cachedInt32ArrayMemory0 = null;
+    cachedUint8ArrayMemory0 = null;
+    wasm.__wbindgen_start();
+    return wasm;
+  }
+  async function __wbg_init(module_or_path) {
+    if (wasm !== void 0) return wasm;
+    if (typeof module_or_path !== "undefined") {
+      if (Object.getPrototypeOf(module_or_path) === Object.prototype) {
+        ({ module_or_path } = module_or_path);
+      } else {
+        console.warn("using deprecated parameters for the initialization function; pass a single object instead");
+      }
+    }
+    if (typeof module_or_path === "undefined") {
+      module_or_path = new URL("rust_app_bg.wasm", import_meta.url);
+    }
+    const imports = __wbg_get_imports();
+    if (typeof module_or_path === "string" || typeof Request === "function" && module_or_path instanceof Request || typeof URL === "function" && module_or_path instanceof URL) {
+      module_or_path = fetch(module_or_path);
+    }
+    __wbg_init_memory(imports);
+    const { instance, module } = await __wbg_load(await module_or_path, imports);
+    return __wbg_finalize_init(instance, module);
+  }
+  var rust_app_default = __wbg_init;
+
+  // <stdin>
+  rust_app_default(wasm_path).then((_) => {
+    function encrypt() {
+      if (!document.getElementById("imnotarobot").checked) {
+        return;
+      }
+      var message = document.getElementById("reason").value + document.getElementById("message").value;
+      var encryptedMessage = encrypt_message(message);
+      const email = "root@trinami.org";
+      const subject = "Encrypted message";
+      const body = encryptedMessage;
+      const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+      window.location.href = mailtoLink;
+    }
+    let button = document.getElementById("encryptButton");
+    button.addEventListener("click", () => {
+      encrypt();
+    });
+  });
+})();
